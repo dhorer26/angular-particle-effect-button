1674fc80f1c03df9ff4a8fded51b5b8bbb22d745
Ravindra Dhore <ravindrad@ansh.com>
2019-11-26 14:20:39 +0530

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,4 +4,29 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {}
+export class AppComponent {
+  reset0: any;
+  reset1: any;
+  reset2: any;
+  reset3: any;
+  reset4: any;
+  reset5: any;
+  reset6: any;
+  reset7: any;
+  reset8: any;
+  reset9: any;
+  reset10: any;
+  reset11: any;
+  hidden0: any;
+  hidden1: any;
+  hidden2: any;
+  hidden3: any;
+  hidden4: any;
+  hidden5: any;
+  hidden6: any;
+  hidden7: any;
+  hidden8: any;
+  hidden9: any;
+  hidden10: any;
+  hidden11: any;
+}
